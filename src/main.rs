ffc1d171508e543b85d3365fec625a4d6540033d
poisnada <horberlan@gmail.com>
2025-04-13 20:57:27 -0300

    feat: add icon suport

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use std::path::Path;
 use std::process::Command;
 use std::rc::Rc;
 
+use xdgkit::icon_finder;
+use resvg::tiny_skia;
+use resvg::usvg;
+
 slint::include_modules!();
 
 #[derive(Deserialize, Debug, Clone)]
@@ -53,26 +57,53 @@ fn focus_client(address: &str) {
 
 fn get_icon_for_class(class: &str) -> Option<Image> {
     let lowercase = class.to_lowercase();
-    // todo: change to a real icon way...
-    let icon_path = format!("/usr/share/icons/hicolor/48x48/apps/{}.png", lowercase);
-
-    if Path::new(&icon_path).exists() {
+    let icon_path = icon_finder::find_icon(lowercase, 48, 1)?;
+    println!("path do icone: {}", &icon_path.to_string_lossy());
+    
+    if icon_path.extension().map_or(false, |ext| ext == "svg") {
+        return load_svg_image(&icon_path);
+    } else {
         if let Ok(reader) = ImageReader::open(&icon_path) {
             if let Ok(img) = reader.decode() {
                 let rgba_img = img.to_rgba8();
                 let (width, height) = rgba_img.dimensions();
-
-                let mut buffer = SharedPixelBuffer::<slint::Rgba8Pixel>::new(width, height);
+                let mut buffer =
+                    SharedPixelBuffer::<slint::Rgba8Pixel>::new(width, height);
                 let buffer_slice = buffer.make_mut_slice();
                 let raw_data = rgba_img.into_raw();
-
                 buffer_slice.copy_from_slice(bytemuck::cast_slice(&raw_data));
-
                 return Some(Image::from_rgba8(buffer));
             }
         }
     }
     None
+}
+
+fn load_svg_image(path: &Path) -> Option<Image> {
+    let svg_data = std::fs::read(path).ok()?;
+    
+    let opt = usvg::Options::default();
+    let tree = usvg::Tree::from_data(&svg_data, &opt).ok()?;
+    
+    let pixmap_size = 48;
+    
+    let mut pixmap = tiny_skia::Pixmap::new(pixmap_size, pixmap_size)?;
+    
+    resvg::render(
+        &tree,
+        usvg::FitTo::Size(pixmap_size, pixmap_size),
+        resvg::tiny_skia::Transform::default(),
+        pixmap.as_mut()
+    )?;
+    
+    let mut buffer =
+        SharedPixelBuffer::<slint::Rgba8Pixel>::new(pixmap.width(), pixmap.height());
+    let buffer_slice = buffer.make_mut_slice();
+    
+    let pixmap_data = pixmap.data();
+    buffer_slice.copy_from_slice(bytemuck::cast_slice(pixmap_data));
+    
+    Some(Image::from_rgba8(buffer))
 }
 
 fn main() -> Result<(), slint::PlatformError> {
