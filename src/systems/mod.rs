9e10a18c0550d85543f3d76231b79048fc746e5d
poisnada <horberlan@gmail.com>
2025-04-25 21:38:01 -0300

    feat(systems): add HoverAnimationSet for system organization

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,8 +2,12 @@ mod drag;
 mod icon;
 mod title;
 mod camera;
+mod scroll;
+mod animation;
 
 pub use drag::*;
 pub use icon::*;
 pub use title::*;
-pub use camera::*; 
+pub use camera::*;
+pub use scroll::*;
+pub use animation::*; 
